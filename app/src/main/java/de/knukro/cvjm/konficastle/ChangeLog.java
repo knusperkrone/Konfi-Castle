839184019563bd20688543a880b822eade2b57c0
knusperkrone <knukro@localhost.localdomain>
2017-01-09 22:38:43 +0100

    You can add notices now

---
+++ app/src/main/java/de/knukro/cvjm/konficastle/ChangeLog.java
@@ -0,0 +1,8 @@
+package de.knukro.cvjm.konficastle;
+
+/**
+ * Created by knukro on 1/9/17.
+ */
+
+public class ChangeLog {
+}
---
+++ app/src/main/java/de/knukro/cvjm/konficastle/NotizenActivity.java
@@ -0,0 +1,15 @@
+package de.knukro.cvjm.konficastle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import de.knukro.cvjm.konficastle.R;
+
+public class NotizenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notizen);
+    }
+}
